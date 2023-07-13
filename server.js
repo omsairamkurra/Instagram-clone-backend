c717e26e5c0799e1781298def7fde5094a580e47
Ak8582 <Kurraomsairam2@gmail.com>
2023-07-13 13:30:33 +0530

    update

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 // Define routes
+// Create a new user
 app.post('/users', async (req, res) => {
   try {
     const user = new User(req.body);
@@ -31,6 +32,55 @@ app.post('/users', async (req, res) => {
     res.status(201).json(user);
   } catch (err) {
     res.status(400).json({ error: err.message });
+  }
+});
+
+// Get all users
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to retrieve users' });
+  }
+});
+
+// Get a specific user
+app.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to retrieve user' });
+  }
+});
+
+// Update a user
+app.put('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to update user' });
+  }
+});
+
+// Delete a user
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to delete user' });
   }
 });
 
